84f2a5f452b5468c208f9349cd590085f8af164f
Max Struijs <maxstruys32@gmail.com>
2023-04-17 11:03:49 +0200

    Updated the code to send mail using the UI

--- main.go
+++ main.go
@@ -1,50 +1,74 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"gopkg.in/gomail.v2"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Email Interface")
+	// Create a new Fyne application
+	myApp := app.New()
 
-	w.Resize(fyne.NewSize(800, 600))
+	// Create a new window with a title and size
+	myWindow := myApp.NewWindow("Email Sender")
+	myWindow.Resize(fyne.Size{Width: 600, Height: 500})
 
-	// Create text boxes for email fields
-	toEntry := widget.NewEntry()
-	toEntry.SetPlaceHolder("To")
+	// Create input fields for the email message
+	fromInput := widget.NewEntry()
+	fromInput.SetPlaceHolder("From")
 
-	ccEntry := widget.NewEntry()
-	ccEntry.SetPlaceHolder("CC")
+	toInput := widget.NewEntry()
+	toInput.SetPlaceHolder("To")
 
-	subjectEntry := widget.NewEntry()
-	subjectEntry.SetPlaceHolder("Subject")
+	subjectInput := widget.NewEntry()
+	subjectInput.SetPlaceHolder("Subject")
 
-	bodyEntry := widget.NewMultiLineEntry()
+	bodyInput := widget.NewMultiLineEntry()
+	bodyInput.SetPlaceHolder("Write your message here :)")
 
-	// Create buttons
+	// Create a button to send the email
 	sendButton := widget.NewButton("Send", func() {
-		// Send email functionality here
+		// Create a new email message
+		m := gomail.NewMessage()
+
+		// Set the sender and recipient email addresses
+		m.SetHeader("From", fromInput.Text)
+		m.SetHeader("To", toInput.Text)
+
+		// Set the email subject and body
+		m.SetHeader("Subject", subjectInput.Text)
+		m.SetBody("text/plain", bodyInput.Text)
+
+		// Create a new mailer object with the SMTP server configuration
+		d := gomail.NewDialer("smtp.gmail.com", 587, "[Email Here]", "[App-password here]")
+
+		// Send the email message
+		if err := d.DialAndSend(m); err != nil {
+			fmt.Println("Failed to send email:", err)
+		} else {
+			fmt.Println("Email sent successfully!")
+		}
 	})
 
-	cancelButton := widget.NewButton("Cancel", func() {
-		// Close window functionality here
-		w.Close()
-	})
+	// Create a container to hold the input fields and button
+	content := container.NewVBox(
+		fromInput,
+		toInput,
+		subjectInput,
+		bodyInput,
+		layout.NewSpacer(),
+		sendButton,
+	)
 
-	// Create container for buttons
-	buttonBox := container.NewHBox(sendButton, cancelButton)
+	// Set the container as the window's content
+	myWindow.SetContent(content)
 
-	// Create container for text boxes
-	entryBox := container.NewVBox(toEntry, ccEntry, subjectEntry, bodyEntry)
-
-	// Create container for all elements
-	content := container.NewVBox(entryBox, buttonBox)
-
-	// Set the window content and show the window
-	w.SetContent(content)
-	w.ShowAndRun()
+	// Show the window and run the Fyne application
+	myWindow.ShowAndRun()
 }
